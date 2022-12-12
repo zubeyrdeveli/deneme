3af644fd1cf5693c991a1fde3299faba97852182
Zubeyr <wassup32@hotmail.com>
2022-12-12 19:34:44 +0300

    day 38

---
+++ src/day38_Inheritance/carTask/Toyota.java
@@ -0,0 +1,16 @@
+package day38_Inheritance.carTask;
+
+public class Toyota extends Car {
+    public Toyota(String model, int year, double price, String color, int miles) {
+        super("Toyota", model, year, price, color, miles);
+    }
+
+    public void reliable () {
+        System.out.println(brand+ " " + model+ " is reliable");
+    }
+
+
+
+
+
+}

8feba26556fee80020cbbf7486cfa0254dc5194c
Zubeyr <wassup32@hotmail.com>
2022-12-12 19:43:25 +0300

    day 38

---
+++ src/day38_Inheritance/carTask/Tesla.java
@@ -0,0 +1,16 @@
+package day38_Inheritance.carTask;
+
+public class Tesla extends Car {
+
+
+    public Tesla(String model, int year, double price, String color, int miles) {
+        super("Tesla", model, year, price, color, miles);
+    }
+
+    public void autoPilot () {
+        System.out.println(brand+ " " + model+ " has autopilot mode");
+    }
+
+
+
+}
